aaa26baefaffcc6db25155e7c2b13b6bbe28c3ae
Gamma <max@brosky.net>
2023-03-01 13:28:03 +0100

    latest version desktop

---
+++ Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasysteem
+{
+    internal class Customer
+    {
+        public string Firstname { get; private set; }
+        public string Lastname { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Customer(string firstname, string lastname, string email, string password, DateTime birthdate)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+            Password = password;
+            Birthdate = birthdate;
+        }
+    }
+}
