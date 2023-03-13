c67b061185fa93368c335e684ad3e5383200a3d3
Gamma <max@brosky.net>
2023-03-13 17:32:09 +0100

    before major change

--- Login.cs
+++ Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -108,6 +109,12 @@ namespace Kassasysteem
         {
             Hide();
             CashregisterForm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HelloDal dal = new HelloDal();
+            MessageBox.Show(dal.HelloDAL());
         }
     }
 }
